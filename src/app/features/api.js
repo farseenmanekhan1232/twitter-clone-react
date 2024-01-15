7d805d9307bde18c1cb27996490d42eb04aafa2a
farseen <farseenmanekhan1232@gmail.com>
2024-01-15 18:46:46 +0530

    updated Api'

--- src/app/features/api.js
+++ src/app/features/api.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
 export const getData = async (endpoint) => {
-  const response = await axios(`http://localhost:3001/${endpoint}`);
+  const response = await axios(
+    `https://twitter-clone-api-2dtc.onrender.com/${endpoint}`
+  );
   return response.data;
 };
 
 export const postData = async (endpoint, data) => {
   const response = await axios(
-    `http://localhost:3001/${endpoint}`,
+    `https://twitter-clone-api-2dtc.onrender.com/${endpoint}`,
     data
       ? {
           method: "POST",
